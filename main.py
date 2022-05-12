d6fca041622c69796cde5c4df4f9621515dd1050
Erick Roberto Martinez <31717695+leonrkmtz@users.noreply.github.com>
2022-05-12 18:54:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+####################################
+#Write your code below this line 👇
+a = int(two_digit_number[0])
+b = int(two_digit_number[1])
+print(a+b)
+
+#Matematical operators:
+# + suma 
+# - resta
+# * multiplicacion
+# / division
+# when you divide, you will always end up with a float ex 4/2: 2.0
+# ** potencia
+
+# PEMDAS
+# aprentessis ()
+# exponent  **
+# multiplication *
+# division /
+# adition +
+# substraction -
+
+height = int(input("please enter your height in meters"))
+weight = int(input("please enter your weight in kg"))
+print (height)
+print (weight)
